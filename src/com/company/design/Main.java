d288a34c4de4b3a2213f492e688f1987cdce2b8a
iniline <1004dlagywls@naver.com>
2022-06-02 16:48:35 +0900

    어댑터 패턴

--- src/com/company/design/Main.java
+++ src/com/company/design/Main.java
@@ -1,5 +1,6 @@
 package com.company.design;
 
+import com.company.design.adapter.*;
 import com.company.design.singleton.Aclazz;
 import com.company.design.singleton.Bclazz;
 import com.company.design.singleton.SocketClient;
@@ -7,14 +8,31 @@ import com.company.design.singleton.SocketClient;
 public class Main {
 
     public static void main(String[] args) {
-        Aclazz aClazz = new Aclazz();
-        Bclazz bClazz = new Bclazz();
+//        Aclazz aClazz = new Aclazz();
+//        Bclazz bClazz = new Bclazz();
+//
+//        SocketClient aClient = aClazz.getSocketClient();
+//        SocketClient bClient = bClazz.getSocketClient();
+//
+//        System.out.println("두 개의 객체가 동일한가?");
+//        System.out.println(aClient.equals(bClient));
 
-        SocketClient aClient = aClazz.getSocketClient();
-        SocketClient bClient = bClazz.getSocketClient();
+        HairDryer hairDryer = new HairDryer();
+        connect(hairDryer);
 
-        System.out.println("두 개의 객체가 동일한가?");
-        System.out.println(aClient.equals(bClient));
+        Cleaner cleaner = new Cleaner();
+        Electronic110V adapter = new SocketAdapter(cleaner);
+        connect(adapter);
+
+        AirConditioner airConditioner = new AirConditioner();
+        Electronic110V airAdapter = new SocketAdapter(airConditioner);
+        connect(airAdapter);
 
     }
+
+    //    콘센트
+    public static void connect(Electronic110V electronic110V) {
+        electronic110V.powerOn();
+    }
+
 }
